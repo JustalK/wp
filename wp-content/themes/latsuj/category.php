0414d6c7250917c8a14bdb51977a22df1c4b15a0
Latsuj <justal.kevin@gmail.com>
2020-07-09 21:04:09 +0800

    [REFACTORING] Separating the mobile from web for category

--- wp-content/themes/latsuj/category.php
+++ wp-content/themes/latsuj/category.php
@@ -9,50 +9,85 @@ $random_posts = get_random_posts_by_category($cat_id,4);
 
 ?>
 
-<?php if ( !wp_is_mobile() ) { ?>
-	<span id="design_side_left" class="design_side">&nbsp;</span>
-	<span id="design_side_right" class="design_side">&nbsp;</span>
-<?php } ?>
-
 <?php wpse_get_partial('template-parts/nav'); ?>
 
-<div id="category">
-	<span id="design_top" class="design">&nbsp;</span> <span
-	id="design_bottom" class="design">&nbsp;</span>
-		
-	<h1><?= $category->cat_name; ?></h1>
-	
-	<div id="theexcerpt">
-    	<p>
-    		<?= $category->category_description; ?>
-    	</p>
-	</div>
-	
-    <?php 
-    wpse_get_partial('template-parts/most_recent_posts', array(
-        'nbr_of_post' => 3,
-        'offset' => 0,
-        'direction' => 'horizontal',
-        'title' => 'Recent Posts'
-    )); ?>
-    <?php wpse_get_partial('template-parts/slider', array(
-        'description' => "Older posts",
-        'first_post' => $posts[3],
-        'second_post' => $posts[4],
-        'third_post' => $posts[5]
-    )); ?>
-    <?php wpse_get_partial('template-parts/preview_posts', array(
-        'post' => $random_posts[0]
-    )); ?>
-    
-    <?php wpse_get_partial('template-parts/slider', array(
-        'description' => "Random posts",
-        'first_post' => $random_posts[1],
-        'second_post' => $random_posts[2],
-        'third_post' => $random_posts[3]
-    )); ?>
+<?php if ( wp_is_mobile() ) : ?>
 
-</div>
+    <div id="category">
+    	<span id="design_top" class="design">&nbsp;</span> <span
+    	id="design_bottom" class="design">&nbsp;</span>
+    		
+    	<h1><?= $category->cat_name; ?></h1>
+    	
+    	<div id="theexcerpt">
+        	<p>
+        		<?= $category->category_description; ?>
+        	</p>
+    	</div>
+    	
+        <?php 
+        wpse_get_partial('template-parts/most_recent_posts', array(
+            'nbr_of_post' => 3,
+            'offset' => 0,
+            'direction' => 'horizontal',
+            'title' => 'Recent Posts'
+        ));
+        wpse_get_partial('template-parts/most_recent_posts', array(
+            'nbr_of_post' => 4,
+            'offset' => 3,
+            'direction' => 'vertical',
+            'title' => 'Older Posts'
+        ));
+        wpse_get_partial('template-parts/preview_posts', array(
+            'post' => $random_posts[0]
+        ));
+        ?>
+    
+    </div>
+
+<?php else : ?>
+	<span id="design_side_left" class="design_side">&nbsp;</span>
+	<span id="design_side_right" class="design_side">&nbsp;</span>
+
+    <div id="category">
+    	<span id="design_top" class="design">&nbsp;</span> <span
+    	id="design_bottom" class="design">&nbsp;</span>
+    		
+    	<h1><?= $category->cat_name; ?></h1>
+    	
+    	<div id="theexcerpt">
+        	<p>
+        		<?= $category->category_description; ?>
+        	</p>
+    	</div>
+    	
+        <?php 
+        wpse_get_partial('template-parts/most_recent_posts', array(
+            'nbr_of_post' => 3,
+            'offset' => 0,
+            'direction' => 'horizontal',
+            'title' => 'Recent Posts'
+        )); ?>
+        <?php wpse_get_partial('template-parts/slider', array(
+            'description' => "Older posts",
+            'first_post' => $posts[3],
+            'second_post' => $posts[4],
+            'third_post' => $posts[5]
+        )); ?>
+        <?php wpse_get_partial('template-parts/preview_posts', array(
+            'post' => $random_posts[0]
+        )); ?>
+        
+        <?php wpse_get_partial('template-parts/slider', array(
+            'description' => "Random posts",
+            'first_post' => $random_posts[1],
+            'second_post' => $random_posts[2],
+            'third_post' => $random_posts[3]
+        )); ?>
+    
+    </div>
+
+<?php endif; ?>
 
 <?php
 get_footer();
