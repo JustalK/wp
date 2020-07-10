68bce47b14625f0de4a1ed126c49192a55fd43a8
Latsuj <justal.kevin@gmail.com>
2020-07-10 19:06:06 +0800

    [FEATURE] Adding the footer

--- wp-content/themes/latsuj/footer.php
+++ wp-content/themes/latsuj/footer.php
@@ -1,4 +1,11 @@
     <footer class="site-footer">
+    	<span class="legend"><?php bloginfo( 'name' ); ?></span>
+    	<div id="bsocials">
+    		<i class="fa fa-facebook-square"></i>
+    		<i class="fa fa-github-square"></i>
+    		<i class="fa fa-youtube-square"></i>
+    	</div>
+    	<span class="copyright">Copyright © <?= date("Y"); ?><br /><br /> Actually, you could use anything and this website. We do not really care. We are not a AAA company, we wont sue you. <br /><br />Peace and Love<br />Happy browsing <3</span>
     </footer>
      
 </div> <!-- closes <div class=container"> --> 
