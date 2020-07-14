2613c2a76e070218a6df1427ba98fc9b36534530
Latsuj <justal.kevin@gmail.com>
2020-07-14 18:33:03 +0800

    [FEATURE] Changing the back button on singular to back to category with highest post

--- wp-content/themes/latsuj/template-parts/nav.php
+++ wp-content/themes/latsuj/template-parts/nav.php
@@ -1,8 +1,19 @@
 <div id="nav">
-    <a class="options" href="javascript:history.back()">
-    	<i class="material-icons">arrow_back</i>
-    	<span>Back</span>	
-    </a>
+	<?php if(is_singular()) {?>
+		<?php 
+            $categories = get_the_category(get_the_ID()); 
+            $category_with_most_posts = $categories[0];
+		?>
+        <a class="options" href="<?= get_category_link($category_with_most_posts->cat_ID) ?>">
+        	<i class="material-icons">arrow_back</i>
+        	<span><?= $category_with_most_posts->cat_name ?></span>	
+        </a>
+    <?php } else { ?>
+		<a class="options" href="javascript:history.back()">
+        	<i class="material-icons">arrow_back</i>
+        	<span>Back</span>	
+        </a>
+    <?php } ?>
     <a class="options" href="<?= home_url(); ?>">
     	<span>Home</span>
     	<i class="material-icons">home</i>
