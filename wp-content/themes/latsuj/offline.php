93defa61eac1edfa4c25650885ab63e36f1cc264
Latsuj <justal.kevin@gmail.com>
2020-07-30 22:01:19 +0800

    [FEATURE] Adding the button for closing all the articles

--- wp-content/themes/latsuj/offline.php
+++ wp-content/themes/latsuj/offline.php
@@ -16,7 +16,7 @@
     ));
     ?>
 
-  <span class="nav-offline bottom">CLOSE ARTICLE</span>
+  <span class="nav-offline bottom">CLOSE ARTICLES</span>
 <?php else : ?>
 
 <span id="design_side_left" class="design_side">&nbsp;</span>
@@ -30,17 +30,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     const posts = document.querySelectorAll(".posts");
     const posts_content = document.querySelectorAll(".posts_content");
+    const close_posts = document.querySelector(".nav-offline.bottom");
     let posts_open =0;
 
-    const show_text = function(e) {
+    const click_close_posts = function(e) {
+      posts_open = 0;
+      close_posts.classList.remove("show");
+      for(let i=posts_content.length;i--;) {
+        posts_content[i].classList.add("hidden");
+      }
+    }
+    close_posts.addEventListener("click", click_close_posts);
+
+    const click_show_text = function(e) {
       const selected_posts = e.currentTarget;
       const id = selected_posts.dataset.id;
       posts_content[id].classList.remove("hidden");
+      if(!posts_open) close_posts.classList.add("show");
       posts_open++;
     }
-
     for(let i=posts.length;i--;) {
-      posts[i].addEventListener("click", show_text);
+      posts[i].addEventListener("click", click_show_text);
     }
 })
 </script>
