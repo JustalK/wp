a45a378e0439189b5bf8c9baa90283488a384a88
Latsuj <justal.kevin@gmail.com>
2020-08-13 20:39:08 +0800

    [FEATURE] Reencoding the font and fixing the glyph

--- wp-content/themes/latsuj/index.php
+++ wp-content/themes/latsuj/index.php
@@ -10,8 +10,8 @@
 		<a href="https://www.youtube.com/channel/UCvmYjxbiFavUv9lipG1pezg" target="_blank"><i class="fa fa-youtube-square"></i></a>
 	</div>
 
-	<?php 
-	
+	<?php
+
 	wpse_get_partial('template-parts/header');
     wpse_get_partial('template-parts/categories_listing');
     wpse_get_partial('template-parts/most_recent_posts', array(
@@ -29,9 +29,9 @@
         'direction' => 'vertical',
         'title' => 'Older Posts'
     ));
-    
+
     ?>
-	<a id="mb-see-all" href="<?php echo esc_url( get_permalink( get_page_by_path( 'posts' ) ) ); ?>">See all <i class="material-icons">chevron_right</i></a>
+	<a id="mb-see-all" href="<?php echo esc_url( get_permalink( get_page_by_path( 'posts' ) ) ); ?>">See all<i class="fa fa-arrow-right"></i></a>
 
 
 <?php else : ?>
@@ -40,32 +40,32 @@
 <span id="design_side_right" class="design_side">&nbsp;</span>
 
     <?php wpse_get_partial('template-parts/header'); ?>
-	<?php wpse_get_partial('template-parts/categories_listing'); ?> 
+	<?php wpse_get_partial('template-parts/categories_listing'); ?>
     <?php wpse_get_partial('template-parts/last_posts'); ?>
-    
+
     <?php wpse_get_partial('template-parts/most_recent_posts', array(
         'nbr_of_post' => 3,
         'offset' => 1
     )); ?>
-    
+
     <?php wpse_get_partial('template-parts/slider', array(
         'description' => "Older posts",
         'nbr_of_post' => 3,
         'offset' => 4,
         'order' => "date"
     )); ?>
-    
+
     <?php wpse_get_partial('template-parts/youtube_listing'); ?>
-    
+
     <?php wpse_get_partial('template-parts/preview_posts'); ?>
-    
+
     <?php wpse_get_partial('template-parts/slider', array(
         'description' => "Random posts",
         'nbr_of_post' => 3,
         'offset' => 0,
         'order' => "rand"
     )); ?>
-	
+
 
 <?php endif; ?>
 
